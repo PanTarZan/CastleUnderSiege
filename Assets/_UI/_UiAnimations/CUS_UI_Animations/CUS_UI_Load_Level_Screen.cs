7206f664b2df2c88a8f054d97109d4ce9d34b17c
PanTarZan <tarzan104@gmail.com>
2020-02-23 14:42:01 +0100

    async level load added

---
+++ Assets/_UI/_UiAnimations/CUS_UI_Animations/CUS_UI_Load_Level_Screen.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(CanvasGroup))]
+public class CUS_UI_Load_Level_Screen : CUS_UI_Screen
+{
+    #region variables
+    [Header("Timed Screen Properties")]
+
+    public float m_ScreenTime = 0.1f;
+    public UnityEvent onTimeCompleted = new UnityEvent();
+
+    private float startTime;
+    #endregion
+
+    #region MainMethods
+    #endregion
+
+    #region HelperMethods
+
+    public override void StartScreen()
+    {
+        base.StartScreen();
+
+        startTime = Time.time;
+        StartCoroutine(WaitForTime());
+    }
+
+    IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(m_ScreenTime);
+
+
+        if (onTimeCompleted != null)
+        {
+            onTimeCompleted.Invoke();
+        }
+        
+    }
+    #endregion
+}
---
+++ Assets/_UI/_UiAnimations/CUS_UI_Animations/CUS_UI_Screen.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(CanvasGroup))]
+public class CUS_UI_Screen : MonoBehaviour
+{
+    #region variables
+    [Header("Main Properties")]
+
+    public Selectable m_StartSelectable;
+
+    [Header("Screen Events")]
+    public UnityEvent onScreenStart = new UnityEvent();
+    public UnityEvent onScreenClose = new UnityEvent();
+
+    private Animator animator;
+    #endregion
+
+    #region MainMethods
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (m_StartSelectable)
+        {
+            EventSystem.current.SetSelectedGameObject(m_StartSelectable.gameObject);
+        }
+    }
+    #endregion
+
+    #region HelperMethods
+
+    public virtual void StartScreen()
+    {
+        if (onScreenStart != null)
+        {
+            onScreenStart.Invoke();
+        }
+        HandleAnimator("show");
+    }
+    public virtual void CloseScreen()
+    {
+        if (onScreenClose != null)
+        {
+            onScreenClose.Invoke();
+        }
+        HandleAnimator("hide");
+    }
+
+    void HandleAnimator(string aTrigger)
+    {
+        if (animator)
+        {
+            animator.SetTrigger(aTrigger);
+        }
+    }
+    #endregion
+}
