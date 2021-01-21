0f8744e7ca5041c9df1604f8e0a599d8fba79268
pantarzan <tarzan104@gmail.com>
2021-01-21 20:49:33 +0100

    settings are, like... made complete i guess

--- Assets/CUS_SettingSystem.cs
+++ Assets/CUS_SettingSystem.cs
@@ -8,17 +8,17 @@ using UnityEngine.Audio;
 
 public class CUS_SettingSystem : MonoBehaviour
 {
+    public Slider masterSlider;
     public Slider musicSlider;
     public Slider effectsSlider;
-    public Slider otherSlider;
     public Toggle enablePostProcessing;
     public Toggle toggle_2;
+    public float master = -40f;
     public float music = -40f;
     public float effects;
-    public float other;
+    public AudioMixerGroup masterAM;
     public AudioMixerGroup musicAM;
     public AudioMixerGroup effectsAM;
-    public AudioMixerGroup otherAM;
     public bool enablePostProcessing_value;
     public bool toggle_2_value;
 
@@ -37,19 +37,14 @@ public class CUS_SettingSystem : MonoBehaviour
     void Update()
     {
         handlePostProcessing();
-        handleAudioMixersProcessing();
-    }
-
-    private void handleAudioMixersProcessing()
-    {
-
     }
 
     private void SetSettingsToUI()
     {
+        Debug.Log("Setting already achieved settings in scene: " + Application.loadedLevelName);
+        masterSlider.value = master;
         musicSlider.value = music;
         effectsSlider.value = effects;
-        otherSlider.value = other;
         enablePostProcessing.isOn = enablePostProcessing_value;
         toggle_2.isOn = toggle_2_value;
 
@@ -58,7 +53,11 @@ public class CUS_SettingSystem : MonoBehaviour
 
     private void handlePostProcessing()
     {
-        Camera.main.GetComponent<PostProcessingBehaviour>().enabled = enablePostProcessing_value;
+        if (Camera.main.GetComponent<PostProcessingBehaviour>().enabled != enablePostProcessing_value)
+        {
+            Camera.main.GetComponent<PostProcessingBehaviour>().enabled = enablePostProcessing_value;
+            Debug.Log("PostProcessing changed to: " + enablePostProcessing_value);
+        }
     }
 
     public void SwitchPostProcessing()
@@ -66,9 +65,19 @@ public class CUS_SettingSystem : MonoBehaviour
         enablePostProcessing_value = enablePostProcessing.isOn;
     }
 
+    public void SetMasterSound(float soundLevel)
+    {
+        masterAM.audioMixer.SetFloat("MasterValue", soundLevel);
+        master = soundLevel;
+    }
     public void SetMusicSound(float soundLevel)
     {
-        musicAM.audioMixer.SetFloat("MusicValue", soundLevel);
-        music = soundLevel;
+        masterAM.audioMixer.SetFloat("MusicValue", soundLevel);
+        master = soundLevel;
+    }
+    public void SetEffectsSound(float soundLevel)
+    {
+        masterAM.audioMixer.SetFloat("EffectsValue", soundLevel);
+        master = soundLevel;
     }
 }
